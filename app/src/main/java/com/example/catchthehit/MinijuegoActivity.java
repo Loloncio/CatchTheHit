f07a7012c4e8b605d0320ebbfc97d9662ace10ab
Loloncio <alejandro.delacruz.garijo@gmail.com>
2022-11-11 18:17:49 +0100

    Vistas Álvaro
    Incluidas los xml de las actividades Minijuego, defensa y movimiento además de los recursos necesarios

---
+++ app/src/main/java/com/example/catchthehit/MinijuegoActivity.java
@@ -0,0 +1,30 @@
+package com.example.pruebaa3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MinijuegoActivity extends AppCompatActivity {
+
+    private ImageButton btnBolaBuena;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minijuego);
+
+        btnBolaBuena = (ImageButton)findViewById(R.id.bola_buena1);
+    }
+
+    public void clickBotonBolaBuena(View v) {
+        //Creamos el intent
+        Intent intent = new Intent(MinijuegoActivity.this, MovimientoActivity.class);
+
+        //Iniciamos la nueva actividad
+        startActivity(intent);
+    }
+}
