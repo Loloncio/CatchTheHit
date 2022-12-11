d111e869d24505ecb1bc27df548a17848e6d5e6f
Loloncio <alejandro.delacruz.garijo@gmail.com>
2022-12-11 22:48:19 +0100

    Bloqueada rotacion de pantalla
    La rotación de pantalla está desactivada.
    
    El test no funciona bien todavía.

--- app/src/main/java/es/uva/inf/smov/catchthehit/Test.java
+++ app/src/main/java/es/uva/inf/smov/catchthehit/Test.java
@@ -2,11 +2,6 @@ package es.uva.inf.smov.catchthehit;
 
 import static androidx.constraintlayout.widget.ConstraintLayoutStates.TAG;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.content.ContextCompat;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
@@ -19,6 +14,10 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -28,9 +27,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
-import es.uva.inf.smov.catchthehit.datos.Jugador;
 import es.uva.inf.smov.catchthehit.datos.Partida;
 
 public class Test extends AppCompatActivity {
@@ -56,7 +53,7 @@ public class Test extends AppCompatActivity {
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
 
-        layout = (ViewGroup) findViewById(R.id.preguntas);
+        layout = findViewById(R.id.preguntas);
         respuestas = new ArrayList<Integer>(18);
 
         myRef.addValueEventListener(new ValueEventListener() {
@@ -65,12 +62,10 @@ public class Test extends AppCompatActivity {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
                 partida = dataSnapshot.getValue(Partida.class);
-                for(int i = 0; i < 4; i++){
 
-                }
-                for(int i = 0; i < 4; i++){
+                for (int i = 0; i < 4; i++) {
                     partida.getEquipo1().elegirJugador(i).setReady(false);
-                    if(partida.getEquipo1().elegirJugador(i).getUsuario().equals(user.getUid())) {
+                    if (partida.getEquipo1().elegirJugador(i).getUsuario().equals(user.getUid())) {
                         jugador = i;
                     }
                 }
@@ -86,56 +81,46 @@ public class Test extends AppCompatActivity {
     }
 
     private void IntroducePreguntas() {
-        int jugadores[];
-        if(jugador==0)
-            jugadores = new int[]{1, 2, 3};
-        else if(jugador==1)
-            jugadores = new int[]{0, 2, 3};
-        else if(jugador==2)
-            jugadores = new int[]{0, 1, 3};
-        else
-            jugadores = new int[]{0, 1, 2};
+        int[] jugadores;
+        if (jugador == 0) jugadores = new int[]{1, 2, 3};
+        else if (jugador == 1) jugadores = new int[]{0, 2, 3};
+        else if (jugador == 2) jugadores = new int[]{0, 1, 3};
+        else jugadores = new int[]{0, 1, 2};
         ArrayList<Integer> preguntas = preguntas();
-        for(int i : preguntas) {
-            Log.e("PreguntasNumero", String.valueOf(i));
+        for (int i : preguntas) {
             LayoutInflater inflater = LayoutInflater.from(this);
             int id = R.layout.pregunta;
 
             ConstraintLayout constraint = (ConstraintLayout) inflater.inflate(id, null, false);
-            TextView pregunta = (TextView) constraint.findViewById(R.id.pregunta);
-            TextView nPregunta = (TextView) constraint.findViewById(R.id.nPregunta);
-            nPregunta.setText(String.valueOf(i+1));
+            TextView pregunta = constraint.findViewById(R.id.pregunta);
+            TextView nPregunta = constraint.findViewById(R.id.nPregunta);
+            nPregunta.setText(String.valueOf(i + 1));
             pregunta.setText(partida.getPregunta(i));
-            Log.e("PreguntasNumero", partida.getPregunta(i));
-            TextView j1 = (TextView) constraint.findViewById(R.id.j1);
-            TextView j2 = (TextView) constraint.findViewById(R.id.j2);
-            TextView j3 = (TextView) constraint.findViewById(R.id.j3);
+            TextView j1 = constraint.findViewById(R.id.j1);
+            TextView j2 = constraint.findViewById(R.id.j2);
+            TextView j3 = constraint.findViewById(R.id.j3);
             j1.setText(partida.getEquipo1().elegirJugador(jugadores[0]).getNombre());
             j2.setText(partida.getEquipo1().elegirJugador(jugadores[1]).getNombre());
             j3.setText(partida.getEquipo1().elegirJugador(jugadores[2]).getNombre());
 
-            ImageView i1 = (ImageView) constraint.findViewById(R.id.J1);
-            ImageView i2 = (ImageView) constraint.findViewById(R.id.J2);
-            ImageView i3 = (ImageView) constraint.findViewById(R.id.J3);
+            ImageView i1 = constraint.findViewById(R.id.J1);
+            ImageView i2 = constraint.findViewById(R.id.J2);
+            ImageView i3 = constraint.findViewById(R.id.J3);
             i1.setImageResource(getImagen(jugadores[0]));
             i2.setImageResource(getImagen(jugadores[1]));
             i3.setImageResource(getImagen(jugadores[2]));
-
-            LinearLayout op1 = (LinearLayout) findViewById(R.id.op2);
-            LinearLayout op2 = (LinearLayout) findViewById(R.id.op2);
-            LinearLayout op3 = (LinearLayout) findViewById(R.id.op2);
-            op1.setTag(String.valueOf(jugadores[0]));
-            op2.setTag(String.valueOf(jugadores[1]));
-            op3.setTag(String.valueOf(jugadores[2]));
+            i1.setTag(jugadores[0]);
+            i2.setTag(jugadores[1]);
+            i3.setTag(jugadores[2]);
 
 
             layout.addView(constraint);
         }
     }
 
-    public void clickFinalizarTest(View v){
+    public void clickFinalizarTest(View v) {
         sinRespuesta = 0;
-        for(int i = 0;i < 18;i++){
+        for (int i = 0; i < 18; i++) {
             if (respuestas.get(i) == null) {
                 sinRespuesta++;
             }
@@ -149,7 +134,7 @@ public class Test extends AppCompatActivity {
             intent.putExtra("codigo", codigo);
 
             startActivity(intent);
-        } else{
+        } else {
             CharSequence fail = "Selecciona un jugador en todas las preguntas";
             Toast toast = Toast.makeText(getApplicationContext(), fail, Toast.LENGTH_LONG);
             toast.show();
@@ -157,60 +142,62 @@ public class Test extends AppCompatActivity {
 
     }
 
-    public void clickOpcion1(View v){
-        LinearLayout op1 = (LinearLayout) v.findViewById(R.id.op1);
+    public void clickOpcion1(View v) {
+        LinearLayout op1 = v.findViewById(R.id.op1);
 
         op1.setBackground(ContextCompat.getDrawable(this, R.drawable.border_test));
         op1.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.negroTranslucido)));
 
-        ConstraintLayout padre = (ConstraintLayout) (ViewGroup) v.getParent();
-        LinearLayout op2 = (LinearLayout) padre.findViewById(R.id.op2);
-        LinearLayout op3 = (LinearLayout) padre.findViewById(R.id.op3);
+        ConstraintLayout padre = (ConstraintLayout) v.getParent();
+        LinearLayout op2 = padre.findViewById(R.id.op2);
+        LinearLayout op3 = padre.findViewById(R.id.op3);
         op2.setBackgroundColor(0x00FFFFFF);
         op3.setBackgroundColor(0x00FFFFFF);
 
-        TextView nPregunta = (TextView) padre.findViewById(R.id.nPregunta);
+        TextView nPregunta = padre.findViewById(R.id.nPregunta);
         int numero = Integer.parseInt(nPregunta.getText().toString());
         Log.e("numero", String.valueOf(numero));
-        respuestas.add(numero-1,Integer.parseInt(op1.getTag().toString()));
+        respuestas.add(numero - 1, Integer.parseInt(op1.getTag().toString()));
     }
-    public void clickOpcion2(View v){
-        LinearLayout op2 = (LinearLayout) v.findViewById(R.id.op2);
+
+    public void clickOpcion2(View v) {
+        LinearLayout op2 = v.findViewById(R.id.op2);
 
         op2.setBackground(ContextCompat.getDrawable(this, R.drawable.border_test));
         op2.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.negroTranslucido)));
 
-        ConstraintLayout padre = (ConstraintLayout) (ViewGroup) v.getParent();
-        LinearLayout op1 = (LinearLayout) padre.findViewById(R.id.op1);
-        LinearLayout op3 = (LinearLayout) padre.findViewById(R.id.op3);
+        ConstraintLayout padre = (ConstraintLayout) v.getParent();
+        LinearLayout op1 = padre.findViewById(R.id.op1);
+        LinearLayout op3 = padre.findViewById(R.id.op3);
         op1.setBackgroundColor(0x00FFFFFF);
         op3.setBackgroundColor(0x00FFFFFF);
 
-        TextView nPregunta = (TextView) padre.findViewById(R.id.nPregunta);
+        TextView nPregunta = padre.findViewById(R.id.nPregunta);
         int numero = Integer.parseInt(nPregunta.getText().toString());
         Log.e("numero", String.valueOf(numero));
-        respuestas.add(numero-1,Integer.parseInt(op1.getTag().toString()));
+        respuestas.add(numero - 1, Integer.parseInt(op1.getTag().toString()));
     }
-    public void clickOpcion3(View v){
-        LinearLayout op3 = (LinearLayout) v.findViewById(R.id.op3);
+
+    public void clickOpcion3(View v) {
+        LinearLayout op3 = v.findViewById(R.id.op3);
 
         op3.setBackground(ContextCompat.getDrawable(this, R.drawable.border_test));
         op3.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.negroTranslucido)));
 
-        ConstraintLayout padre = (ConstraintLayout) (ViewGroup) v.getParent();
-        LinearLayout op1 = (LinearLayout) padre.findViewById(R.id.op1);
-        LinearLayout op2= (LinearLayout) padre.findViewById(R.id.op2);
+        ConstraintLayout padre = (ConstraintLayout) v.getParent();
+        LinearLayout op1 = padre.findViewById(R.id.op1);
+        LinearLayout op2 = padre.findViewById(R.id.op2);
         op1.setBackgroundColor(0x00FFFFFF);
         op2.setBackgroundColor(0x00FFFFFF);
 
-        TextView nPregunta = (TextView) padre.findViewById(R.id.nPregunta);
+        TextView nPregunta = padre.findViewById(R.id.nPregunta);
         int numero = Integer.parseInt(nPregunta.getText().toString());
         Log.e("numero", String.valueOf(numero));
-        respuestas.add(numero-1,Integer.parseInt(op1.getTag().toString()));
+        respuestas.add(numero - 1, Integer.parseInt(op1.getTag().toString()));
     }
 
-    private int getImagen(int id){
-        switch (id){
+    private int getImagen(int id) {
+        switch (id) {
             case 0:
                 return getResources().getIdentifier("es.uva.inf.smov.catchthehit:drawable/red_player", null, null);
 
@@ -225,33 +212,24 @@ public class Test extends AppCompatActivity {
         }
     }
 
-    private ArrayList<Integer> preguntas(){
+    private ArrayList<Integer> preguntas() {
         ArrayList<Integer> preguntas = new ArrayList<Integer>();
-        int preguntasTest = 18/partida.getRondas();
-        int resto = 18%partida.getRondas();
+        int resto = 18 % partida.getRondas();
         int inicio = 0;
-        int size = 0;
+        int size = 18 / partida.getRondas();
 
-        if (size%partida.getRondas() != 0){
-            for (int i = 0; i<resto;i++)
-                if(partida.getRondaAct() == i)
-                    preguntasTest++;
+        if (resto != 0) {
+            for (int i = 0; i < resto; i++)
+                if (partida.getRondaAct() == i) size++;
         }
-        Log.e("TestPreguntas", String.valueOf(preguntasTest));
 
-        if (size%partida.getRondas() != 0)
-            for (int i = 0;i<partida.getRondas();i++)
-                if (partida.getRondaAct() < resto)
-                    inicio = preguntasTest * partida.getRondaAct();
-                else
-                    inicio = preguntasTest * partida.getRondaAct() + resto;
-        else
-            inicio=size*partida.getRondaAct();
-        Log.e("TestInicio", String.valueOf(preguntasTest));
+        if (size % partida.getRondas() != 0) for (int i = 0; i < partida.getRondas(); i++)
+            if (partida.getRondaAct() < resto) inicio = size * (partida.getRondaAct() - 1);
+            else inicio = size * (partida.getRondaAct() - 1) + resto;
+        else inicio = size * (partida.getRondaAct() - 1);
 
-        for(int i = 0; i < size; i++ ) {
+        for (int i = 0; i < size; i++) {
             preguntas.add(i + inicio);
-            Log.e("TestArray", String.valueOf(i+inicio));
         }
 
         return preguntas;
